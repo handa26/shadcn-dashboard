c48007a55acc1e11bb51d0c106db460ac80e4f00
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-04-05 10:54:00 +0800

    feat: add table action button

--- app/payments/columns.tsx
+++ app/payments/columns.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export type Payment = {
 	id: string;
@@ -52,6 +63,34 @@ export const columns: ColumnDef<Payment>[] = [
 			}).format(amount);
 
 			return <div className="text-right font-medium">{formatted}</div>;
+		},
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const payment = row.original;
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuItem
+							onClick={() => navigator.clipboard.writeText(payment.id)}
+						>
+							Copy payment ID
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>View customer</DropdownMenuItem>
+						<DropdownMenuItem>View payment details</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			);
 		},
 	},
 ];
