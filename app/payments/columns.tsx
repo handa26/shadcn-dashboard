b9b530b6ac7e2505f6d83dd184753d4d400c339e
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-04-05 11:20:35 +0800

    feat: table row selection

--- app/payments/columns.tsx
+++ app/payments/columns.tsx
@@ -14,6 +14,7 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export type Payment = {
 	id: string;
@@ -24,6 +25,28 @@ export type Payment = {
 };
 
 export const columns: ColumnDef<Payment>[] = [
+	{
+		id: "select",
+		header: ({ table }) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && "indeterminate")
+				}
+				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+				aria-label="Select all"
+			/>
+		),
+		cell: ({ row }) => (
+			<Checkbox
+				checked={row.getIsSelected()}
+				onCheckedChange={(value) => row.toggleSelected(!!value)}
+				aria-label="Select row"
+			/>
+		),
+		enableSorting: false,
+		enableHiding: false,
+	},
 	{
 		accessorKey: "username",
 		header: "Username",
--- app/payments/data-table.tsx
+++ app/payments/data-table.tsx
@@ -33,6 +33,7 @@ export function DataTable<TData, TValue>({
 	data,
 }: DataTableProps<TData, TValue>) {
 	const [sorting, setSorting] = React.useState<SortingState>([]);
+	const [rowSelection, setRowSelection] = React.useState({});
 
 	const table = useReactTable({
 		data,
@@ -40,10 +41,12 @@ export function DataTable<TData, TValue>({
 		getCoreRowModel: getCoreRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
 		onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    state: {
-      sorting,
-    },
+		getSortedRowModel: getSortedRowModel(),
+		onRowSelectionChange: setRowSelection,
+		state: {
+			sorting,
+			rowSelection,
+		},
 	});
 
 	return (
