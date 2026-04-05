e398f6ee0b54ac36c3eff0f6050e0fd1a17fd17a
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-04-05 10:49:10 +0800

    feat: data table with tanstack, create custom headers and cells

---
+++ app/payments/columns.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { cn } from "@/lib/utils";
+
+export type Payment = {
+	id: string;
+	amount: number;
+	status: "pending" | "processing" | "success" | "failed";
+	email: string;
+	username: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+	{
+		accessorKey: "username",
+		header: "Username",
+	},
+	{
+		accessorKey: "email",
+		header: "Email",
+	},
+	{
+		accessorKey: "status",
+		header: "Status",
+		cell: ({ row }) => {
+			const status = row.getValue("status");
+
+			return (
+				<div
+					className={cn(
+						`p-1 rounded-md w-max text-xs`,
+						status === "pending" && "bg-yellow-500/40",
+						status === "success" && "bg-green-500/40",
+						status === "failed" && "bg-red-500/40",
+					)}
+				>
+					{status as string}
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "amount",
+		header: () => <div className="text-right">Amount</div>,
+		cell: ({ row }) => {
+			const amount = parseFloat(row.getValue("amount"));
+			const formatted = new Intl.NumberFormat("en-US", {
+				style: "currency",
+				currency: "USD",
+			}).format(amount);
+
+			return <div className="text-right font-medium">{formatted}</div>;
+		},
+	},
+];
